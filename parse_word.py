f6808d3ef3cc9f32ebe7db52b30db46489b6b95b
M. Willis Monroe <willismonroe@gmail.com>
2017-03-06 13:58:24 -0800

    Add stress finding algorithm, also fix edge-case in syllabification

--- parse_word.py
+++ parse_word.py
@@ -1,12 +1,13 @@
 # Akkadian vowels and consonants
-vowels = ['a', 'e', 'i', 'u']
+short_vowels = ['a', 'e', 'i', 'u']
+macron_vowels = ['ā', 'ē', 'ī', 'ū']
+circumflex_vowels = ['â', 'ê', 'î', 'û']
+
 consonants = ['b', 'd', 'g', 'ḫ', 'k', 'l', 'm',
               'n', 'p', 'q', 'r', 's', 'ṣ', 'š',
               't', 'ṭ', 'w', 'y', 'z', 'ʾ']
 
-vowels_with_length = ['ā', 'ē', 'ī', 'ū', 'â', 'ê', 'î', 'û']
-
-vowels = vowels + vowels_with_length
+vowels = short_vowels + macron_vowels + circumflex_vowels
 
 
 def get_syllables(word):
@@ -14,23 +15,26 @@ def get_syllables(word):
     Convert normalized word to a list of syllables.
     This function will not analyze stress.
 
-    The general logic follows Huehnergard 3rd edition:
+    The general logic follows Huehnergard 3rd edition (pg. 3):
     (a) Every syllable has one, and only one, vowel.
     (b) With two exceptions, no syllable may begin with a vowel. The exceptions
     are: the beginning of a word; the second of two successive vowels (note:
     some scholars prefer to write ʾ between any two vowels in a word: e.g.,
     kiʾam rather than our kiam).
     (c) No syllable may begin or end with two consonants.
+    :param word: a string in Akkadian
+    :return: a list of syllables
     """
     syllables = []
 
     # Rule (b.ii)
-    if word[0] in vowels:
+    if word[0] in vowels and word[2] not in consonants:
         syllables.append(word[0])
         word = word[1:]
 
     # flip the word and count from the back:
     word = word[::-1]
+
     syllables_reverse = []
     i = 0
     while i < len(word):
@@ -47,17 +51,133 @@ def get_syllables(word):
         # "ma" -> "am"
         elif char in consonants:
             if word[i + 1] in vowels:
-                if word[i + 2] in consonants:
+                # If there are only two syllables left, that's it
+                if i + 2 >= len(word):
+                    syllables_reverse.append(word[i + 1] + word[i])
+                    break
+                elif word[i + 2] in consonants:
                     syllables_reverse.append(word[i + 2] + word[i + 1] + word[i])
                     i += 3
                 elif word[i + 2] in vowels:
                     syllables_reverse.append(word[i + 1] + word[i])
                     i += 2
+        else:
+            i += 1
 
     return syllables + syllables_reverse[::-1]
 
 
-def test():
+def find_stress(word):
+    """
+    Find the stressed syllable in a word.
+
+    The general logic follows Huehnergard 3rd edition (pgs. 3-4):
+    (a) Light: ending in a short vowel: e.g., -a, -ba
+    (b) Heavy: ending in a long vowel marked with a macron, or in a
+    short vowel plus a consonant: e.g., -ā, -bā, -ak, -bak
+    (c) Ultraheavy: ending in a long vowel marked with a circumflex,
+    in any long vowel plus a consonant: e.g., -â, -bâ, -āk, -bāk, -âk, -bâk.
+
+    (a) If the last syllable is ultraheavy, it bears the stress.
+    (b) Otherwise, stress falls on the last non-final heavy or ultraheavy syllable.
+    (c) Words that contain no non-final heavy or ultraheavy syllables have the
+    stress fall on the first syllable.
+
+    :param word: a string (or list) in Akkadian
+    :return: a list of syllables with stressed syllable surrounded by "[]"
+    """
+    if type(word) is str:
+        word = get_syllables(word)
+
+    syllables_stress = []
+    for i, syllable in enumerate(word):
+        # enumerate over the syllables and mark them for length
+
+        # Ultraheavy:
+        # -â, -bâ, -āk, -bāk, -âk, -bâk.
+        if len(syllable) == 1:
+            if syllable in circumflex_vowels:
+                syllables_stress.append((syllable, "Ultraheavy"))
+                continue
+        elif len(syllable) == 2:
+            if syllable[0] in consonants and syllable[1] in circumflex_vowels:
+                syllables_stress.append((syllable, "Ultraheavy"))
+                continue
+            if syllable[0] in macron_vowels + circumflex_vowels and syllable[1] in consonants:
+                syllables_stress.append((syllable, "Ultraheavy"))
+                continue
+        elif len(syllable) == 3:
+            if syllable[1] in macron_vowels + circumflex_vowels:
+                syllables_stress.append((syllable, "Ultraheavy"))
+                continue
+
+        # Heavy:
+        # -ā, -bā, -ak, -bak
+        if len(syllable) == 1:
+            if syllable in macron_vowels:
+                syllables_stress.append((syllable, "Heavy"))
+                continue
+        elif len(syllable) == 2:
+            if syllable[0] in consonants and syllable[1] in macron_vowels:
+                syllables_stress.append((syllable, "Heavy"))
+                continue
+            if syllable[0] in short_vowels and syllable[1] in consonants:
+                syllables_stress.append((syllable, "Heavy"))
+                continue
+        elif len(syllable) == 3:
+            if syllable[1] in short_vowels:
+                syllables_stress.append((syllable, "Heavy"))
+                continue
+
+        # Short:
+        # -a, -ba
+        if len(syllable) == 1:
+            if syllable in short_vowels:
+                syllables_stress.append((syllable, "Light"))
+                continue
+        elif len(syllable) == 2:
+            if syllable[0] in consonants and syllable[1] in short_vowels:
+                syllables_stress.append((syllable, "Light"))
+                continue
+
+    # It's easier to find stress backwards
+    syllables_stress = syllables_stress[::-1]
+
+    syllables = []
+    found_stress = 0
+    for i, syllable in enumerate(syllables_stress):
+        # If we've found the stressed syllable just append the next syllable
+        if found_stress:
+            syllables.append(syllable[0])
+            continue
+
+        # Rule (a)
+        elif syllable[1] == "Ultraheavy" and i == 0:
+            syllables.append("[{}]".format(syllable[0]))
+            found_stress = 1
+            continue
+
+        # Rule (b)
+        elif syllable[1] in ['Ultraheavy', 'Heavy'] and i > 0:
+            syllables.append("[{}]".format(syllable[0]))
+            found_stress = 1
+            continue
+
+        else:
+            syllables.append(syllable[0])
+
+    # Reverse the list again
+    syllables = syllables[::-1]
+
+    # If we still haven't found stress then rule (c) applies
+    # Rule (c)
+    if not found_stress:
+        syllables[0] = "[{}]".format(syllables[0])
+
+    return syllables
+
+
+def test_syllabification():
     # small test suite
     # TODO: move this out to a real test system
     print(get_syllables('balāṭī') == ['ba', 'lā', 'ṭī'])
@@ -68,3 +188,20 @@ def test():
     print(get_syllables('epištašu') == ['e', 'piš', 'ta', 'šu'])
     print(get_syllables('kiam') == ['ki', 'am'])
     print(get_syllables('kiʾam') == ['ki', 'ʾam'])
+
+    # The syllabification rules fail on this example from the stress rules
+    print(get_syllables('ibnû') == ['ib', 'nû'])
+
+
+def test_stress():
+    print(find_stress('ibnû') == ['ib', '[nû]'])
+    print(find_stress('idūk') == ['i', '[dūk]'])
+    print(find_stress('iparras') == ['i', '[par]', 'ras'])
+    print(find_stress('nidittum') == ['ni', '[dit]', 'tum'])
+    print(find_stress('idūkū') == ['i', '[dū]', 'kū'])
+    print(find_stress('tēteneppušā') == ['tē', 'te', '[nep]', 'pu', 'šā'])
+    print(find_stress('itâršum') == ['i', '[târ]', 'šum'])
+    print(find_stress('napištašunu') == ['na', '[piš]', 'ta', 'šu', 'nu'])
+    print(find_stress('zikarum') == ['[zi]', 'ka', 'rum'])
+    print(find_stress('šunu') == ['[šu]', 'nu'])
+    print(find_stress('ilū') == ['[i]', 'lū'])
